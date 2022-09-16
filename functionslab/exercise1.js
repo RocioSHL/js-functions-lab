ad67341d3f23fb3a6090f587b26490818e8be43d
Rocio Sanchez <20101172@mail.wit.ie>
2022-09-16 20:00:41 +0100

    Exercise 1

---
+++ functionslab/exercise1.js
@@ -0,0 +1,13 @@
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then(response => response.json())
+  .then(todos => {
+     const completed = todos.filter(todo => todo.completed==true)  // Complete this code
+     completed.forEach( (todo, index) => {
+     
+      {console.log(`${todo.title} - ${todo.completed}`)
+     }
+    })
+  })
+  .catch(function(err) { 
+    console.log(err);
+  });
